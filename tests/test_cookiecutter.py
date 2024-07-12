65197db27f376a1a9b4ee39bb521cd29e180b29f
Julien Carsique <julien.carsique@sonarsource.com>
2024-07-12 19:13:45 +0200

    ci(BUILD-5443): use Cirrus CI

--- tests/test_cookiecutter.py
+++ tests/test_cookiecutter.py
@@ -1,15 +1,19 @@
+import pathlib
+
+
 def test_defaults(cookies):
     result = cookies.bake()
 
     assert result.exit_code == 0
     assert result.exception is None
-    assert result.project.basename == 'A repository'
-    assert result.project.isdir()
-    assert len(result.project.listdir()) == 3
-    assert result.project.join('README.md').isfile()
-    assert result.project.join('LICENSE').isfile()
-    assert result.project.join('.github', 'CODEOWNERS').isfile()
-    assert len(result.project.join('.github', 'workflows').listdir()) == 0
+    project_path: pathlib.Path = result.project_path
+    assert project_path.name == 'A repository'
+    assert project_path.is_dir()
+    assert len(list(project_path.iterdir())) == 3
+    assert project_path.joinpath('README.md').is_file()
+    assert project_path.joinpath('LICENSE').is_file()
+    assert project_path.joinpath('.github', 'CODEOWNERS').is_file()
+    assert len(list(project_path.joinpath('.github', 'workflows').iterdir())) == 0
 
 
 def test_customization(cookies):
@@ -25,10 +29,11 @@ def test_customization(cookies):
 
     assert result.exit_code == 0
     assert result.exception is None
-    assert result.project.basename == 'Test repository'
-    assert result.project.isdir()
-    assert len(result.project.listdir()) == 7
-    assert "test-team" in result.project.join('.github', 'CODEOWNERS').read()
-    assert "Test description" in result.project.join('README.md').read()
-    assert "GNU LESSER GENERAL PUBLIC LICENSE" in result.project.join('LICENSE').read()
-    assert len(result.project.join('.github', 'workflows').listdir()) == 2
+    project_path: pathlib.Path = result.project_path
+    assert project_path.name == 'Test repository'
+    assert project_path.is_dir()
+    assert len(list(project_path.iterdir())) == 7
+    assert "test-team" in project_path.joinpath('.github', 'CODEOWNERS').read_text()
+    assert "Test description" in project_path.joinpath('README.md').read_text()
+    assert "GNU LESSER GENERAL PUBLIC LICENSE" in project_path.joinpath('LICENSE').read_text()
+    assert len(list(project_path.joinpath('.github', 'workflows').iterdir())) == 2
